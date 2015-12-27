ca5c83eb7050af9d7a9988578da8212670a86132
Jane Alexander <jalexanderny@earthlink.net>
2015-12-27 12:01:42 -0500

    jeopardy game

---
+++ js/various.js
@@ -0,0 +1,641 @@
+
+
+/*$(document).ready(function() {
+   $('div').click(function() {
+     $('.amount').fadeOut('slow');
+     $('.questions').fadeIn('slow');
+     });
+ });
+ 
+$(document).ready(function() {
+   $('.questions').click(function() {
+     $('.questions').fadeOut('slow');
+     $('.answers').fadeIn('9000');
+   });
+ });
+
+$(document).ready(function() {
+   $('.answers').click(function() {
+     $('.answers').fadeOut('slow');
+     
+   });
+ });*/
+
+/*may do the following as a case statement?*/
+/*first row*/
+ $(document).ready(function() {
+   $('#amount1').click(function() {
+     $('#amount1').fadeOut('slow');
+     	$('#answer1').fadeIn('slow');
+     });
+ });
+ $(document).ready(function() {
+   $('#answer1').click(function() {
+     $('#answer1').fadeOut('slow');
+     	$('#question1').fadeIn('slow');
+     	});
+ });
+
+ $(document).ready(function() {
+   $('#question1').click(function() {
+     $('#question1').fadeOut('slow');
+     });
+ });
+
+
+ $(document).ready(function() {
+   $('#amount2').click(function() {
+     $('#amount2').fadeOut('slow');
+     	$('#answer2').fadeIn('slow');
+     });
+ });
+ $(document).ready(function() {
+   $('#answer2').click(function() {
+     $('#answer2').fadeOut('slow');
+     	$('#question2').fadeIn('slow');
+     	});
+ });
+
+ $(document).ready(function() {
+   $('#question2').click(function() {
+     $('#question2').fadeOut('slow');
+     });
+ });
+
+
+
+ $(document).ready(function() {
+   $('#amount3').click(function() {
+     $('#amount3').fadeOut('slow');
+     	$('#answer3').fadeIn('slow');
+     });
+ });
+ $(document).ready(function() {
+   $('#answer3').click(function() {
+     $('#answer3').fadeOut('slow');
+     	$('#question3').fadeIn('slow');
+     	});
+ });
+
+ $(document).ready(function() {
+   $('#question3').click(function() {
+     $('#question3').fadeOut('slow');
+     });
+ });
+
+
+ $(document).ready(function() {
+   $('#amount4').click(function() {
+     $('#amount4').fadeOut('slow');
+     	$('#answer4').fadeIn('slow');
+     });
+ });
+ $(document).ready(function() {
+   $('#answer4').click(function() {
+     $('#answer4').fadeOut('slow');
+     	$('#question4').fadeIn('slow');
+     	});
+ });
+
+ $(document).ready(function() {
+   $('#question4').click(function() {
+     $('#question4').fadeOut('slow');
+     });
+ });
+
+
+
+ $(document).ready(function() {
+   $('#amount5').click(function() {
+     $('#amount5').fadeOut('slow');
+     	$('#answer5').fadeIn('slow');
+     });
+ });
+ $(document).ready(function() {
+   $('#answer5').click(function() {
+     $('#answer5').fadeOut('slow');
+     	$('#question5').fadeIn('slow');
+     	});
+ });
+
+ $(document).ready(function() {
+   $('#question5').click(function() {
+     $('#question5').fadeOut('slow');
+     });
+ });
+
+
+
+ $(document).ready(function() {
+   $('#amount6').click(function() {
+     $('#amount6').fadeOut('slow');
+     	$('#answer6').fadeIn('slow');
+     });
+ });
+ $(document).ready(function() {
+   $('#answer6').click(function() {
+     $('#answer6').fadeOut('slow');
+     	$('#question6').fadeIn('slow');
+     	});
+ });
+
+ $(document).ready(function() {
+   $('#question6').click(function() {
+     $('#question6').fadeOut('slow');
+     });
+ });
+/*end of first row*/ 
+
+/*second row*/
+ $(document).ready(function() {
+   $('#amount7').click(function() {
+     $('#amount7').fadeOut('slow');
+     	$('#answer7').fadeIn('slow');
+     });
+ });
+ $(document).ready(function() {
+   $('#answer7').click(function() {
+     $('#answer7').fadeOut('slow');
+     	$('#question7').fadeIn('slow');
+     	});
+ });
+
+ $(document).ready(function() {
+   $('#question7').click(function() {
+     $('#question7').fadeOut('slow');
+     });
+ });
+
+
+
+ $(document).ready(function() {
+   $('#amount8').click(function() {
+     $('#amount8').fadeOut('slow');
+     	$('#answer8').fadeIn('slow');
+     });
+ });
+ $(document).ready(function() {
+   $('#answer8').click(function() {
+     $('#answer8').fadeOut('slow');
+     	$('#question8').fadeIn('slow');
+     	});
+ });
+
+ $(document).ready(function() {
+   $('#question8').click(function() {
+     $('#question8').fadeOut('slow');
+     });
+ });
+
+
+ $(document).ready(function() {
+   $('#amount9').click(function() {
+     $('#amount9').fadeOut('slow');
+     	$('#answer9').fadeIn('slow');
+     });
+ });
+ $(document).ready(function() {
+   $('#answer9').click(function() {
+     $('#answer9').fadeOut('slow');
+     	$('#question9').fadeIn('slow');
+     	});
+ });
+
+ $(document).ready(function() {
+   $('#question9').click(function() {
+     $('#question9').fadeOut('slow');
+     });
+ });
+ 
+
+ 
+ $(document).ready(function() {
+   $('#amount10').click(function() {
+     $('#amount10').fadeOut('slow');
+     	$('#answer10').fadeIn('slow');
+     });
+ });
+ $(document).ready(function() {
+   $('#answer10').click(function() {
+     $('#answer10').fadeOut('slow');
+     	$('#question10').fadeIn('slow');
+     	});
+ });
+
+ $(document).ready(function() {
+   $('#question10').click(function() {
+     $('#question10').fadeOut('slow');
+     });
+ });
+
+
+ $(document).ready(function() {
+   $('#amount11').click(function() {
+     $('#amount11').fadeOut('slow');
+     	$('#answer11').fadeIn('slow');
+     });
+ });
+ $(document).ready(function() {
+   $('#answer11').click(function() {
+     $('#answer11').fadeOut('slow');
+     	$('#question11').fadeIn('slow');
+     	});
+ });
+
+ $(document).ready(function() {
+   $('#question11').click(function() {
+     $('#question11').fadeOut('slow');
+     });
+ });
+
+$(document).ready(function() {
+   $('#amount12').click(function() {
+     $('#amount12').fadeOut('slow');
+     	$('#answer12').fadeIn('slow');
+     });
+ });
+ $(document).ready(function() {
+   $('#answer12').click(function() {
+     $('#answer12').fadeOut('slow');
+     	$('#question12').fadeIn('slow');
+     	});
+ });
+
+ $(document).ready(function() {
+   $('#question12').click(function() {
+     $('#question12').fadeOut('slow');
+     });
+ });
+ /*end of second row*/
+   
+   /*third row*/
+ $(document).ready(function() {
+   $('#amount13').click(function() {
+     $('#amount13').fadeOut('slow');
+     	$('#answer13').fadeIn('slow');
+     });
+ });
+ $(document).ready(function() {
+   $('#answer13').click(function() {
+     $('#answer13').fadeOut('slow');
+     	$('#question13').fadeIn('slow');
+     	});
+ });
+
+ $(document).ready(function() {
+   $('#question13').click(function() {
+     $('#question13').fadeOut('slow');
+     });
+ });
+
+
+
+ $(document).ready(function() {
+   $('#amount14').click(function() {
+     $('#amount14').fadeOut('slow');
+     	$('#answer14').fadeIn('slow');
+     });
+ });
+ $(document).ready(function() {
+   $('#answer14').click(function() {
+     $('#answer14').fadeOut('slow');
+     	$('#question14').fadeIn('slow');
+     	});
+ });
+
+ $(document).ready(function() {
+   $('#question14').click(function() {
+     $('#question14').fadeOut('slow');
+     });
+ });
+
+
+
+ $(document).ready(function() {
+   $('#amount15').click(function() {
+     $('#amount15').fadeOut('slow');
+     	$('#answer15').fadeIn('slow');
+     });
+ });
+ $(document).ready(function() {
+   $('#answer15').click(function() {
+     $('#answer15').fadeOut('slow');
+     	$('#question15').fadeIn('slow');
+     	});
+ });
+
+ $(document).ready(function() {
+   $('#question15').click(function() {
+     $('#question15').fadeOut('slow');
+     });
+ });
+
+
+
+ $(document).ready(function() {
+   $('#amount16').click(function() {
+     $('#amount16').fadeOut('slow');
+     	$('#answer16').fadeIn('slow');
+     });
+ });
+ $(document).ready(function() {
+   $('#answer16').click(function() {
+     $('#answer16').fadeOut('slow');
+     	$('#question16').fadeIn('slow');
+     	});
+ });
+
+ $(document).ready(function() {
+   $('#question16').click(function() {
+     $('#question16').fadeOut('slow');
+     });
+ });
+
+
+
+ $(document).ready(function() {
+   $('#amount17').click(function() {
+     $('#amount17').fadeOut('slow');
+     	$('#answer17').fadeIn('slow');
+     });
+ });
+ $(document).ready(function() {
+   $('#answer17').click(function() {
+     $('#answer17').fadeOut('slow');
+     	$('#question17').fadeIn('slow');
+     	});
+ });
+
+ $(document).ready(function() {
+   $('#question17').click(function() {
+     $('#question17').fadeOut('slow');
+     });
+ });
+
+
+ $(document).ready(function() {
+   $('#amount18').click(function() {
+     $('#amount18').fadeOut('slow');
+     	$('#answer18').fadeIn('slow');
+     });
+ });
+ $(document).ready(function() {
+   $('#answer18').click(function() {
+     $('#answer18').fadeOut('slow');
+     	$('#question18').fadeIn('slow');
+     	});
+ });
+
+ $(document).ready(function() {
+   $('#question18').click(function() {
+     $('#question18').fadeOut('slow');
+     });
+ });
+ /*end of third row*/
+   
+ /*fourth row*/
+ $(document).ready(function() {
+   $('#amount19').click(function() {
+     $('#amount19').fadeOut('slow');
+     	$('#answer19').fadeIn('slow');
+     });
+ });
+ $(document).ready(function() {
+   $('#answer19').click(function() {
+     $('#answer19').fadeOut('slow');
+     	$('#question19').fadeIn('slow');
+     	});
+ });
+
+ $(document).ready(function() {
+   $('#question19').click(function() {
+     $('#question19').fadeOut('slow');
+     });
+ });
+
+
+ $(document).ready(function() {
+   $('#amount20').click(function() {
+     $('#amount20').fadeOut('slow');
+     	$('#answer20').fadeIn('slow');
+     });
+ });
+ $(document).ready(function() {
+   $('#answer20').click(function() {
+     $('#answer20').fadeOut('slow');
+     	$('#question20').fadeIn('slow');
+     	});
+ });
+
+ $(document).ready(function() {
+   $('#question20').click(function() {
+     $('#question20').fadeOut('slow');
+     });
+ });
+
+
+
+ $(document).ready(function() {
+   $('#amount21').click(function() {
+     $('#amount21').fadeOut('slow');
+     	$('#answer21').fadeIn('slow');
+     });
+ });
+ $(document).ready(function() {
+   $('#answer21').click(function() {
+     $('#answer21').fadeOut('slow');
+     	$('#question21').fadeIn('slow');
+     	});
+ });
+
+ $(document).ready(function() {
+   $('#question21').click(function() {
+     $('#question21').fadeOut('slow');
+     });
+ });
+
+
+ $(document).ready(function() {
+   $('#amount22').click(function() {
+     $('#amount22').fadeOut('slow');
+     	$('#answer22').fadeIn('slow');
+     });
+ });
+ $(document).ready(function() {
+   $('#answer22').click(function() {
+     $('#answer22').fadeOut('slow');
+     	$('#question22').fadeIn('slow');
+     	});
+ });
+
+ $(document).ready(function() {
+   $('#question22').click(function() {
+     $('#question22').fadeOut('slow');
+     });
+ });
+
+
+
+ $(document).ready(function() {
+   $('#amount23').click(function() {
+     $('#amount23').fadeOut('slow');
+     	$('#answer23').fadeIn('slow');
+     });
+ });
+ $(document).ready(function() {
+   $('#answer23').click(function() {
+     $('#answer23').fadeOut('slow');
+     	$('#question23').fadeIn('slow');
+     	});
+ });
+
+ $(document).ready(function() {
+   $('#question23').click(function() {
+     $('#question23').fadeOut('slow');
+     });
+ });
+
+
+
+ $(document).ready(function() {
+   $('#amount24').click(function() {
+     $('#amount24').fadeOut('slow');
+     	$('#answer24').fadeIn('slow');
+     });
+ });
+ $(document).ready(function() {
+   $('#answer24').click(function() {
+     $('#answer24').fadeOut('slow');
+     	$('#question24').fadeIn('slow');
+     	});
+ });
+
+ $(document).ready(function() {
+   $('#question24').click(function() {
+     $('#question24').fadeOut('slow');
+     });
+ });
+/*end of fourth row*/
+/*fifth row*/
+
+ $(document).ready(function() {
+   $('#amount25').click(function() {
+     $('#amount25').fadeOut('slow');
+     	$('#answer25').fadeIn('slow');
+     });
+ });
+ $(document).ready(function() {
+   $('#answer25').click(function() {
+     $('#answer25').fadeOut('slow');
+     	$('#question25').fadeIn('slow');
+     	});
+ });
+
+ $(document).ready(function() {
+   $('#question25').click(function() {
+     $('#question25').fadeOut('slow');
+     });
+ });
+
+
+ $(document).ready(function() {
+   $('#amount26').click(function() {
+     $('#amount26').fadeOut('slow');
+     	$('#answer26').fadeIn('slow');
+     });
+ });
+ $(document).ready(function() {
+   $('#answer26').click(function() {
+     $('#answer26').fadeOut('slow');
+     	$('#question26').fadeIn('slow');
+     	});
+ });
+
+ $(document).ready(function() {
+   $('#question26').click(function() {
+     $('#question26').fadeOut('slow');
+     });
+ });
+
+
+ $(document).ready(function() {
+   $('#amount27').click(function() {
+     $('#amount27').fadeOut('slow');
+     	$('#answer27').fadeIn('slow');
+     });
+ });
+ $(document).ready(function() {
+   $('#answer27').click(function() {
+     $('#answer27').fadeOut('slow');
+     	$('#question27').fadeIn('slow');
+     	});
+ });
+
+ $(document).ready(function() {
+   $('#question27').click(function() {
+     $('#question27').fadeOut('slow');
+     });
+ });
+
+ $(document).ready(function() {
+   $('#amount28').click(function() {
+     $('#amount28').fadeOut('slow');
+     	$('#answer28').fadeIn('slow');
+     });
+ });
+ $(document).ready(function() {
+   $('#answer28').click(function() {
+     $('#answer28').fadeOut('slow');
+     	$('#question28').fadeIn('slow');
+     	});
+ });
+
+ $(document).ready(function() {
+   $('#question28').click(function() {
+     $('#question28').fadeOut('slow');
+     });
+ });
+
+
+ $(document).ready(function() {
+   $('#amount29').click(function() {
+     $('#amount29').fadeOut('slow');
+     	$('#answer29').fadeIn('slow');
+     });
+ });
+ $(document).ready(function() {
+   $('#answer29').click(function() {
+     $('#answer29').fadeOut('slow');
+     	$('#question29').fadeIn('slow');
+     	});
+ });
+
+ $(document).ready(function() {
+   $('#question30').click(function() {
+     $('#question30').fadeOut('slow');
+     });
+ });
+
+
+ $(document).ready(function() {
+   $('#amount30').click(function() {
+     $('#amount30').fadeOut('slow');
+     	$('#answer30').fadeIn('slow');
+     });
+ });
+ $(document).ready(function() {
+   $('#answer30').click(function() {
+     $('#answer30').fadeOut('slow');
+     	$('#question30').fadeIn('slow');
+     	});
+ });
+
+ $(document).ready(function() {
+   $('#question30').click(function() {
+     $('#question30').fadeOut('slow');
+     });
+ });
+
+ /*end of fifth row*/
+ 
+
